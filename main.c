5c760cc39564f164ea9b3ba3b7f0c8a63bb28638
Luiz Leite <feitosaleitelf@gmail.com>
2017-11-30 01:18:58 -0300

    Debug

--- main.c
+++ main.c
@@ -15,18 +15,18 @@ int main (int argc, char **argv)
 	gpio trig1 = 23;
 	gpio echo1 = 24;
 
-	gpio trig2 = 2;
-	gpio echo2 = 3;
+	gpio trig2 = 27;
+	gpio echo2 = 22;
 
 	float distance1, distance2;
 	ultrasonic_stop (trig1, echo1);
-	//ultrasonic_stop (trig2, echo2);
+	ultrasonic_stop (trig2, echo2);
 
 	servo_reset ();
 
 	printf("Setting up ultrasonic...\n");
 	ultrasonic_setup (trig1, echo1);
-	//ultrasonic_setup (trig2, echo2);
+	ultrasonic_setup (trig2, echo2);
 
 	printf("Setting up servo...\n");
 	servo_setup ();
@@ -34,8 +34,8 @@ int main (int argc, char **argv)
 	printf("System started!\n");
 	while (1) {
 		distance1 = ultrasonic_get_distance (trig1, echo1);
-		//distance2 = ultrasonic_get_distance (trig2, echo2);
-		//printf("%.2f, %.2f\n", distance1, distance2);
+		distance2 = ultrasonic_get_distance (trig2, echo2);
+		printf("%.2f, %.2f\n", distance1, distance2);
 		if ((distance1 < 20.0) /*&& (distance2 < 20.0)*/) {
 			printf("%s\n", "Opening the gate...");
 			servo_set_angle(90);
